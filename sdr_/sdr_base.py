8ecfa992a1b2ec1cf09a515d7cc1abc1bbd2549c
Lorenzo <cavallaro.lory@gmail.com>
2026-04-22 17:11:13 +0200

    sdr live

---
+++ sdr_/sdr_base.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Full sample of grabbing signals
+# Author: Arman Ebrahimi Mehr
+# Copyright: NavSAS
+# GNU Radio version: 3.10.11.0
+
+from gnuradio import blocks
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import osmosdr
+import time
+import threading
+
+
+
+
+class sdr_base(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Full sample of grabbing signals", catch_exceptions=True)
+        self.flowgraph_started = threading.Event()
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 10e6
+        self.buffer_size = buffer_size = 500e-3
+        self.num_samples_buffer = num_samples_buffer = int(buffer_size*samp_rate)
+        self.f_L1 = f_L1 = 1575420000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.osmosdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + "hackrf=0"    # name of sdr
+        )
+        self.osmosdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(f_L1, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(True, 0)
+        self.osmosdr_source_0.set_gain(0, 0)
+        self.osmosdr_source_0.set_if_gain(0, 0)
+        self.osmosdr_source_0.set_bb_gain(0, 0)
+        self.osmosdr_source_0.set_antenna('', 0)
+        self.osmosdr_source_0.set_bandwidth(samp_rate, 0)
+        self.blocks_vector_sink_x_0 = blocks.vector_sink_b(1, 1024)
+        self.blocks_complex_to_interleaved_char_0 = blocks.complex_to_interleaved_char(False, 2**7)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_complex_to_interleaved_char_0, 0), (self.blocks_vector_sink_x_0, 0))
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_complex_to_interleaved_char_0, 0))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_num_samples_buffer(int(self.buffer_size*self.samp_rate))
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.osmosdr_source_0.set_bandwidth(self.samp_rate, 0)
+
+    def get_buffer_size(self):
+        return self.buffer_size
+
+    def set_buffer_size(self, buffer_size):
+        self.buffer_size = buffer_size
+        self.set_num_samples_buffer(int(self.buffer_size*self.samp_rate))
+
+    def get_num_samples_buffer(self):
+        return self.num_samples_buffer
+
+    def set_num_samples_buffer(self, num_samples_buffer):
+        self.num_samples_buffer = num_samples_buffer
+
+    def get_f_L1(self):
+        return self.f_L1
+
+    def set_f_L1(self, f_L1):
+        self.f_L1 = f_L1
+        self.osmosdr_source_0.set_center_freq(self.f_L1, 0)
+
+
+
+
+def main(top_block_cls=sdr_base, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+    tb.flowgraph_started.set()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
---
+++ sdr_/sdr_stream.py
@@ -0,0 +1,87 @@
+import numpy as np
+import collections
+import threading
+from utils.sdr_base import sdr_base
+
+
+class SdrStream(sdr_base):
+    def __init__(self, *args, buffer_size=500e-3, buffer_length=10, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_buffer_size(buffer_size)
+
+        self.iq_buffer = collections.deque(maxlen=buffer_length)  # Circular buffer
+        self.stream_thread = None
+        self.stop_flag = threading.Event()
+        self.iq = None
+
+    def run_background_stream(self):
+        """
+        Run the SDR flowgraph in a separate thread, continuously acquiring data.
+        """
+        self.start()
+        self.flowgraph_started.set()
+        print(f'--- SDR streaming thread started.')
+
+        while not self.stop_flag.is_set():
+            if len(self.blocks_vector_sink_x_0.data()) > 0.5 * self.num_samples_buffer:
+                iq_data = self.blocks_vector_sink_x_0.data()
+                self.iq_buffer.append(iq_data)  # Store in buffer
+
+                # self.blocks_head_0_0.reset()
+                self.blocks_vector_sink_x_0.reset()
+            else:
+                self.stop_flag.wait(0.001)  # Efficient waiting without CPU overuse
+
+        print(f'--- SDR streaming thread finished.')
+        self.stop()
+        self.wait()
+
+    def start_stream(self):
+        """
+        Starts the background SDR stream.
+        """
+        if self.stream_thread is None or not self.stream_thread.is_alive():
+            self.stop_flag.clear()
+            self.stream_thread = threading.Thread(target=self.run_background_stream, daemon=True)
+            self.stream_thread.start()
+
+    def stop_stream(self):
+        """
+        Stops the background SDR stream.
+        """
+        self.stop_flag.set()
+        self.flowgraph_started.clear()  # Ensure it's reset
+        self.iq_buffer.clear()
+        self.stream_thread= None
+
+
+    def get_samples(self, window_chunk=200e-6):
+        """
+        Fetches the latest `num_samples` from the most recent IQ data in the buffer.
+        """
+        if not self.iq_buffer:
+            print("--- No IQ data available!")
+            return None
+
+        num_samples = int(window_chunk * self.samp_rate * 2)
+
+        # Get the last chunk from the buffer
+        iq_raw = np.array(self.iq_buffer[-1], dtype=np.uint8)[-num_samples:]    # use float32
+        iq_data = iq_raw.view(np.int8).astype(np.float32).view(np.complex64)
+
+        if np.array_equal(iq_data, self.iq):  # True
+            print(f'--- IQ data of two streaming are equal.')
+        else:
+            max_length = max(map(len, self.iq_buffer))  # Cleaner & faster!
+            print(f"--- Maximum IQ buffer element length: {max_length}")
+
+        self.iq = iq_data
+        return iq_data
+
+
+    def sig_handler(self):
+        self.stop_flag.set()
+        self.iq_buffer.clear()
+        self.stream_thread = None
+        return True
+
